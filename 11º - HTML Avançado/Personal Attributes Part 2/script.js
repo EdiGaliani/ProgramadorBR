8526d8f2bffeb202af3178c56c2878d45a3f7978
galiano <edigaliano@gmail.com>
2022-09-29 10:49:01 -0300

    Part 2

---
+++ 11º - HTML Avançado/Personal Attributes Part 2/script.js
@@ -0,0 +1,16 @@
+
+
+debugger
+let lista = document.getElementById("lista");
+
+lista.dataset.num = 20;
+
+let num = parseInt(lista.dataset.num);
+
+let conteudo = "";
+
+for(let i = 0; i < num; i++) {
+    conteudo += "<li>" + i + "</li>";
+}
+
+lista.innerHTML = conteudo;
