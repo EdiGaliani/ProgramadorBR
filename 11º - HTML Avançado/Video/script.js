81f00e9fdd8437f6ee329037b24ce359a294391d
galiano <edigaliano@gmail.com>
2022-09-30 11:11:50 -0300

    Video Part 2

---
+++ 11º - HTML Avançado/Video/script.js
@@ -0,0 +1,28 @@
+
+
+let video = document.getElementById("video1");
+
+function retroceder() {
+    video.currentTime -= 5;
+}
+
+function avancar() {
+    video.currentTime += 5;
+}
+
+function diminuir() {
+    video.playbackRate -= 0.1;
+}
+
+function aumentar() {
+    video.playbackRate += 0.1;
+}
+
+function play() {
+    video.play();
+}
+ 
+function stop() {
+    video.pause();
+    video.currentTime = 0;
+ }
