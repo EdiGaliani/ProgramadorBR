1cf682643436fe87e04909b38294820afa2cef4b
galiano <edigaliano@gmail.com>
2022-10-07 11:42:17 -0300

    Using the Strict Mode

---
+++ 14º - Javascript Avançado/Strict Mode/script.js
@@ -0,0 +1,17 @@
+
+
+// O "use strict" Obriga você declarar o var, let ou const dentro do escopo ou fora
+
+"use strict";
+
+ var x = 3;
+
+function imprimir() {
+    console.log(x)
+
+}
+
+
+imprimir();
+//console.log(x)
+console.log(window)
