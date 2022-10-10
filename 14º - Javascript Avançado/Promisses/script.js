f3371c7e60c0110c60e6b7c603f1e75f6766b39c
galiano <edigaliano@gmail.com>
2022-10-10 16:20:48 -0300

    Lerning about Promises

---
+++ 14º - Javascript Avançado/Promisses/script.js
@@ -0,0 +1,32 @@
+
+
+let usuarios = ["Adriano", "Marcia", "José"];
+
+function inserirUsuario(nome) {
+
+    let promise = new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            usuarios.push(nome);
+            let error = true;
+            if(!error) {
+                resolve();
+            }else {
+                reject({msg: "Mensagem de erro"})
+            }
+            
+        }, 1000);
+    }) 
+    return promise;
+    
+}
+
+function listarUsuarios() {
+    console.log(usuarios);
+}
+
+inserirUsuario("Edi")
+    .then(listarUsuarios)
+    .catch((error) => {
+        console.log(error)
+    });
+
