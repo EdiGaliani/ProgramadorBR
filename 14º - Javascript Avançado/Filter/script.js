d23187d992282401c633ae08f559559a08ea7491
galiano <edigaliano@gmail.com>
2022-10-11 18:32:46 -0300

    More Informations About Filter With Loiane Metod

--- 14º - Javascript Avançado/Filter/script.js
+++ 14º - Javascript Avançado/Filter/script.js
@@ -31,4 +31,12 @@ console.log(filtroIdade)
 
 
 
- 
+ function isEven(x) {
+    //console.log(x);
+    return x % 2 === 0 ? true : false
+ }
+
+ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+ const evenNumbers = numbers.filter(isEven);
+ console.log(evenNumbers);
