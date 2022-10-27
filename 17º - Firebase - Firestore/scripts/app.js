d7442456789c772c97668a69789ac1c50391ce6c
galiano <edigaliano@gmail.com>
2022-10-27 11:19:40 -0300

    Starting The Firebase Project

---
+++ 17º - Firebase - Firestore/scripts/app.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdBSlzeZcCPOtCY3lX_gePLz9Ud6wgIwk",
+  authDomain: "colegio-5503b.firebaseapp.com",
+  projectId: "colegio-5503b",
+  storageBucket: "colegio-5503b.appspot.com",
+  messagingSenderId: "247959897539",
+  appId: "1:247959897539:web:30b175e5d6f29142636976",
+  measurementId: "G-6MJGDG0K4E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
