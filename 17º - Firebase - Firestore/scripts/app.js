843ca4103af200ee2a64f541310a2006c47f00b5
galiano <edigaliano@gmail.com>
2022-11-01 14:47:50 -0300

    Reading Documents

--- 17º - Firebase - Firestore/scripts/app.js
+++ 17º - Firebase - Firestore/scripts/app.js
@@ -1,7 +1,4 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
+const config = {
   apiKey: "AIzaSyAdBSlzeZcCPOtCY3lX_gePLz9Ud6wgIwk",
   authDomain: "colegio-5503b.firebaseapp.com",
   projectId: "colegio-5503b",
@@ -10,6 +7,25 @@ const firebaseConfig = {
   appId: "1:247959897539:web:30b175e5d6f29142636976",
   measurementId: "G-6MJGDG0K4E"
 };
+firebase.initializeApp(config);
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+let db = firebase.firestore();
+
+// Lê Todos os dados de uma colecão
+db.collection("turmaA").get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let aluno = doc.data();
+        console.log(aluno.nome);
+        // console.log(doc.data());
+      })
+    });
+
+// Lê 1 registro especifico apenas
+
+let docRef = db.collection("turmaA").doc("9wHzchJMnpqV5tePyLim");
+docRef.get().then((doc) => {
+  let aluno = doc.data();
+  console.log(aluno.nome);
+})
+    
