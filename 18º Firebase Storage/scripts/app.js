752d33074359844fd36091ae2798907a5b82de94
galiano <edigaliano@gmail.com>
2022-11-06 17:05:13 -0300

    Creating a Reference

---
+++ 18º Firebase Storage/scripts/app.js
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfbpsgqMgYtD-fX6vHlzEBxECx3wbxj8E",
+  authDomain: "cursodevweb-ad7f4.firebaseapp.com",
+  projectId: "cursodevweb-ad7f4",
+  storageBucket: "cursodevweb-ad7f4.appspot.com",
+  messagingSenderId: "718380242886",
+  appId: "1:718380242886:web:c77a554a691e6b86e184c7"
+};
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+
+const ref = storage.ref("/images");
+
+// ref.listAll().then(res => {
+//   res.items[0].getDownloadURL().then(url => {
+//     console.log(url);
+//   });
+// })
+
+ref.listAll().then(res => {
+  console.log(res);
+  res.items[0].listAll().then(res => {
+    console.log(res);
+  })
+})
