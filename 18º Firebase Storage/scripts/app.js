69aeb826088e7357357df7f7bb0c2b8326c40820
galiano <edigaliano@gmail.com>
2022-11-07 12:16:31 -0300

    A Little More About Reference

--- 18º Firebase Storage/scripts/app.js
+++ 18º Firebase Storage/scripts/app.js
@@ -9,7 +9,19 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
 
-const ref = storage.ref("/images");
+const ref = storage.ref("/images/WhatsApp Image 2022-10-11 at 16.18.09.jpeg");
+
+ref.root.listAll().then(res => {
+  console.log(res);
+})
+
+
+
+
+
+
+// const fileRef = ref.child("WhatsApp Image 2022-10-11 at 16.18.09.jpeg");
+// fileRef.getDownloadURL().then(url => { console.log(url)} )
 
 // ref.listAll().then(res => {
 //   res.items[0].getDownloadURL().then(url => {
@@ -17,9 +29,9 @@ const ref = storage.ref("/images");
 //   });
 // })
 
-ref.listAll().then(res => {
-  console.log(res);
-  res.items[0].listAll().then(res => {
-    console.log(res);
-  })
-})
+// ref.listAll().then(res => {
+//   console.log(res);
+//   res.items[0].listAll().then(res => {
+//     console.log(res);
+//   })
+// })
