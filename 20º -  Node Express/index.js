3d1d84c1cc4f50e8250a3cf065cf6f1d00467320
galiano <edigaliano@gmail.com>
2022-11-17 10:41:17 -0300

    Static Files With Express

--- 20º -  Node Express/index.js
+++ 20º -  Node Express/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port: ${PORT}`);
