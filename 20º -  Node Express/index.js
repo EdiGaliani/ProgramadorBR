49104316cda9d4e500b6bfd59fb8c21652c2a526
galiano <edigaliano@gmail.com>
2022-11-17 11:11:16 -0300

    GET in Express

--- 20º -  Node Express/index.js
+++ 20º -  Node Express/index.js
@@ -3,7 +3,12 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use("/meusite", express.static(path.join(__dirname, 'client')));
+
+app.get("/", (req, res) => {
+    // res.type("html");
+    res.send("<h1>Hello World From GET</h1>");
+})
 
 app.listen(PORT, () => {
     console.log(`Server Running on Port: ${PORT}`);
