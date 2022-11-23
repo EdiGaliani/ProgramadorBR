ea47a1fc6ff65acf3627182052e322986539430b
galiano <edigaliano@gmail.com>
2022-11-23 14:31:21 -0300

    Creating Notice Wall Part 4

--- 21º  - Node Front With Back/index.js
+++ 21º  - Node Front With Back/index.js
@@ -1,25 +1,13 @@
 const PORT = 3000;
 const express = require('express');
-const bodyParser = require('body-parser'); //Deprecated
-const posts = require('./model/posts');
+const apiRoute = require('./routes/api');
+
+const path = require('path');
 const app = express();
 
+app.use("/api", apiRoute);
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get("/all", (req, res) => {
-    
-    res.json(JSON.stringify(posts.getAll()));
-})
-
-app.post("/new", bodyParser.json(), (req, res) => {
-    
-    let title = req.body.title;
-    let description = req.body.description;
-    console.log(typeof title);
-    posts.newPost(title, description);
-    
-    res.send("Post Adicionado");
-})
 
 app.listen(PORT, () => {
     console.log("Server Running On Port:", PORT)
