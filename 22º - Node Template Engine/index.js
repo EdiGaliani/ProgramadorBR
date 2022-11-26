d36cb9709f9c9fa4553120c3c171e52930dcb9fe
galiano <edigaliano@gmail.com>
2022-11-26 10:32:35 -0300

    Installing And Starting EJS

--- 22º - Node Template Engine/index.js
+++ 22º - Node Template Engine/index.js
@@ -4,27 +4,48 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-let user = {
-    id: 0,
-    name: "Edi",
-    phone: "(18)235-8554"
 
-}
-
-function render(data, obj) {
-    for(let key in obj) {
-        data = data.replace(`{{{${key}}}}`, obj[key]);
-    }
-    return data;
-}
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    fs.readFile('./templates/user.gali', 'UTF8', (err, data) => {
-        if(!err) {
-            res.send(render(data, user));
-        }
-    })
+    res.render("user")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let user = {
+//     id: 0,
+//     name: "Edi",
+//     phone: "(18)235-8554"
+
+// }
+
+// function render(data, obj) {
+//     for(let key in obj) {
+//         data = data.replace(`{{{${key}}}}`, obj[key]);
+//     }
+//     return data;
+// }
+
+// app.get("/", (req, res) => {
+//     fs.readFile('./templates/user.html', 'UTF8', (err, data) => {
+//         if(!err) {
+//             console.log(data, user);
+//             res.send(render(data, user));
+//         }
+//     })
+// })
 
 
 app.listen(PORT, () => {console.log("Server Running on Port:", PORT)})
