9b67620a8356241d5c0611cb445f6ad0d8b3eb4d
galiano <edigaliano@gmail.com>
2022-11-26 11:08:18 -0300

    'For' in EJS

--- 22º - Node Template Engine/index.js
+++ 22º - Node Template Engine/index.js
@@ -4,12 +4,29 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+let users = [
+    {
+        id: 0,
+        name: "Edi Galiano",
+        phone: "(18)8856-6658"
+    }, {
+        id: 1,
+        name: "Paula Galiano",
+        phone: "(18)8856-6658"
+    }, {
+        id: 2,
+        name: "Melissa Galiano",
+        phone: "(18)8856-6658"
+    }
+]
+
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("user", {name: "Edi Galiano", phone: "(17)99855-8857"})
+    res.render("user", {users});
+    // res.render("user", {name: "Edi Galiano", phone: "(17)99855-8857"})
 })
 
 
