b77f509d3fc47102ac19bc637de87ff29c7a322b
galiano <edigaliano@gmail.com>
2022-12-13 10:47:05 -0300

    Creating Documents

--- 24º - MongoDB Mongoose/app.js
+++ 24º - MongoDB Mongoose/app.js
@@ -5,6 +5,28 @@ const PORT = 3000;
 
 mongoose.set('strictQuery', true)
 
+const linkSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    url: String,
+    click: Number
+})
+
+const Link = mongoose.model('Link', linkSchema);
+
+let link = new Link({
+    title: "Youtube",
+    description: "Link para o Youtube",
+    url: "https://youtube.com",
+    click: 0
+})
+
+link.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log(err);
+});
+
 // // 1º Maneira de se connectar
 // mongoose.connect('mongodb://127.0.0.1:27017/blog').then(db => {
 //     console.log(db);
@@ -13,12 +35,12 @@ mongoose.set('strictQuery', true)
 // });
 
 // 2º Maneira de se Conectar
-mongoose.connect('mongodb://127.0.0.1:27017/blog');
+mongoose.connect('mongodb://127.0.0.1:27017/links');
 
 let db = mongoose.connection;
 
 db.on("error", () => { console.log("Houve um erro") });
-db.once("open", () => { console.log("Banco carregado") })
+db.once("open", () => { console.log("Banco carregado") });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
